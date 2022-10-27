a7457bde504df4da381b726ca093a9e6a3a4f6e3
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-10-27 11:37:48 -0500

    v1

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 //morgan
-var morgan = require('morgan')
+var morgan = require('morgan');
 // body-parser
-var bodyParser = require('body-parser')
-var things = require('./things')
+var bodyParser = require('body-parser');
 
-app.use(morgan('dev'))
-app.use('/things', things)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.use(function (req, res, next) {
-  console.log(`The actual date is ${new Date()}`)
-  next()
-})
+  console.log(`The actual date is ${new Date()}`);
+  next();
+});
 
 app.get('/', function (req, res) {
-  res.send({ name: 'pATRICK', age: '24' })
-})
+  res.send({ name: 'pATRICK', age: '24' });
+});
 
 app.get('/:id', function (req, res) {
-  res.send(`You requested ${req.params.id}`)
-})
+  res.send(`You requested ${req.params.id}`);
+});
 
 app.get('*', function (req, res) {
-  res.send('404 not found')
-})
+  res.send('404 not found');
+});
 
 app.post('/hello', function (req, res) {
-  res.send(req.body)
-})
+  res.send(req.body);
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+  console.log('Server is running on port 3000');
+});
